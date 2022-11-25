c28adc05ffb1abf32b204c9329066c9afc546c16
Ioseb1999 <guruli.websmart@gmail.com>
2022-11-25 04:29:34 -0800

    Changed type

--- controllers/Agent.js
+++ controllers/Agent.js
@@ -3,7 +3,7 @@ const Agent = require("../Model/user");
 
 const createAgent = (req, res) => {
     const agent = new Agent({
-      name: req.body.city,
+      name: req.body.name,
       name_eng: req.body.name_eng,
       constact_number: req.body.constact_number,
       photo_url: req.body.photo_url
@@ -28,7 +28,7 @@ const createAgent = (req, res) => {
 
   const deleteAgent = (req, res) => {
     Agent.deleteOne({ _id: req.params.id })
-      .then(() => res.json({ message: "House Deleted" }))
+      .then(() => res.json({ message: "Agent Deleted" }))
       .catch((err) => res.send(err));
   };
 
